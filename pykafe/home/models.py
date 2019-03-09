fe895914910f43b0990eed6dae6382cdd2b494ba
PeteCoward <peter@catalpa.io>
2019-03-09 11:58:46 +0900

    Add hoome page editable image

--- pykafe/home/models.py
+++ pykafe/home/models.py
@@ -3,12 +3,20 @@ from django.db import models
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
-
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 class HomePage(Page):
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+')
+
     body = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
+        ImageChooserPanel('image'),
         FieldPanel('body', classname="full"),
     ]
 
