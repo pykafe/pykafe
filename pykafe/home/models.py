b941c6ec53aab11f5d1bad3752fb7c4cb95de1ee
PeteCoward <peter@catalpa.io>
2019-03-16 10:40:36 +0900

    Editable pykafe rich blocks

--- pykafe/home/models.py
+++ pykafe/home/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
+from wagtail.core import blocks
 from wagtail.core.models import Page
-from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.fields import RichTextField, StreamField
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
+
+from .blocks import PykafeRichBlock
 
 class HomePage(Page):
     image = models.ForeignKey(
@@ -13,11 +16,11 @@ class HomePage(Page):
         on_delete=models.SET_NULL,
         related_name='+')
 
-    body = RichTextField(blank=True)
+    body = StreamField([('text', PykafeRichBlock())])
 
     content_panels = Page.content_panels + [
         ImageChooserPanel('image'),
-        FieldPanel('body', classname="full"),
+        StreamFieldPanel('body'),
     ]
 
 
