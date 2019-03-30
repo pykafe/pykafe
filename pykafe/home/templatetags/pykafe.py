57a2bae313998b9e01fda03204b8e3fddfc6e817
Onorio de Jesus Afonso <onorio@catalpa.io>
2019-03-30 11:44:00 +0900

    remove underline at the link in header

--- pykafe/home/templatetags/pykafe.py
+++ pykafe/home/templatetags/pykafe.py
@@ -2,9 +2,15 @@ from django import template
 
 register = template.Library()
 
+
 @register.simple_tag(takes_context=True)
 def get_site_root(context):
     # This returns a core.Page. The main menu needs to have the site.root_page
     # defined else will return an object attribute error ('str' object has no
     # attribute 'get_children')
     return context['request'].site.root_page
+
+
+@register.filter
+def startswith(value, arg):
+    return value.startswith(arg)
