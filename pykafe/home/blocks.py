d445e0d543ebb74c5a4ecf575b469564d6ebbf69
joanico <joanicobarros@gmail.com>
2019-04-06 13:12:08 +0900

    modified rich blog

--- pykafe/home/blocks.py
+++ pykafe/home/blocks.py
@@ -13,14 +13,7 @@ class blogRichBlock(blocks.StructBlock):
     """Blog with image and text button(s)"""
     title = blocks.CharBlock(required=True, help_text="Add your title")
     blogs = blocks.ListBlock(
-        blocks.StructBlock(
-            [
-                ("title", blocks.CharBlock(required=True, max_length=22)),
-                ("image", ImageChooserBlock(required=True)),
-                ("text", blocks.TextBlock(required=True, max_length=400)),
-                ("button_Page", blocks.PageChooserBlock(required=False)),
-            ]
-        )
+               blocks.PageChooserBlock(target_model ="home.BasePage")
     )
 
     class Meta:
