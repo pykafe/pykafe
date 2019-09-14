ebb1d1f36d857ebc615c6a56fca64244fa678aad
Mariano de Deus <dedeus_mariano@yahoo.com>
2019-09-14 17:39:03 +0900

    hatama translate

--- pykafe/analytic/wagtail_hooks.py
+++ pykafe/analytic/wagtail_hooks.py
@@ -1,9 +1,4 @@
 
-from wagtail.contrib.modeladmin.options import (
-    ModelAdmin,
-    ModelAdminGroup,
-    modeladmin_register
-)
 from tracking.models import Visitor
 from django.utils.translation import ugettext_lazy as _
 
@@ -35,18 +30,6 @@ def register_admin_urls():
     ]
 
 
-class VisitorAdmin(ModelAdmin):
-    model = Visitor
-    menu_label = "Visitor"
-    menu_icon = "pick"
-    menu_order = 300
-    add_to_settings_menu = False
-    exclude_from_explorer = False
-    list_display = ('session_key', 'user', 'start_time', 'ip_address', 'user_agent')
-    list_filter = ('user', 'ip_address')
-    search_fields = ('user',)
-
-
 @hooks.register('register_admin_menu_item')
 def register_styleguide_menu_item():
     return MenuItem(
@@ -55,5 +38,3 @@ def register_styleguide_menu_item():
         classnames='icon icon-fa-bar-chart',
         order=1000
     )
-
-modeladmin_register(VisitorAdmin)
