ea15d6ffe5d0b5b46c209cd6a1cca451e8903c27
Mariano de Deus <dedeus_mariano@yahoo.com>
2019-10-25 11:31:28 +0900

    Hadia 502 bad gateway

--- pykafe/analytic/views.py
+++ pykafe/analytic/views.py
@@ -5,41 +5,35 @@ from geoip2.errors import AddressNotFoundError
 
 
 def dashboard(request):
-    dict_view = {}
+    page_view = {}
     visitor_view = {}
     country_name = {}
     count_users = {}
     geolocation = {}
     g = GeoIP2()
-    for page in Pageview.objects.all():
-        for filterpage in Pageview.objects.filter(url=page.url).distinct():
-            dict_view.update({ filterpage.url: Pageview.objects.filter(url=page.url).count()})
+    for page in Pageview.objects.distinct():
+        pages =  Pageview.objects.filter(url=page.url)
+        try:
+            page_view[page.url] += pages.count()
+        except KeyError as e:
+            page_view.update({page.url: pages.count()})
     for visitor in Visitor.objects.all():
         try:
             visitor_view[(visitor.start_time).strftime("%Y-%m-%d")] += [visitor.session_key]
-        except KeyError:
-            visitor_view.update({(visitor.start_time).strftime("%Y-%m-%d"): [visitor.session_key]})
-        try:
             country_name[g.city(visitor.ip_address)['country_name']] += [g.city(visitor.ip_address)['country_name']]
-        except (KeyError, AddressNotFoundError) as e:
-            try:
-                country_name.update({g.city(visitor.ip_address)['country_name']: [g.city(visitor.ip_address)['country_name']]})
-            except (KeyError, AddressNotFoundError) as e:
-                pass
-        try:
             count_users[visitor.user] += [visitor.user]
-        except KeyError:
-             count_users.update({visitor.user: [visitor.user]})
-        try:
             geolocation[g.city(visitor.ip_address)['country_name']] += [g.city(visitor.ip_address)['country_name'], g.city(visitor.ip_address)['latitude'], g.city(visitor.ip_address)['longitude'] ]
         except (KeyError, AddressNotFoundError) as e:
             try:
+                visitor_view.update({(visitor.start_time).strftime("%Y-%m-%d"): [visitor.session_key]})
+                country_name.update({g.city(visitor.ip_address)['country_name']: [g.city(visitor.ip_address)['country_name']]})
                 geolocation.update({g.city(visitor.ip_address)['country_name']: [g.city(visitor.ip_address)['country_name'], g.city(visitor.ip_address)['latitude'], g.city(visitor.ip_address)['longitude']]})
+                count_users.update({visitor.user: [visitor.user]})
             except (KeyError, AddressNotFoundError) as e:
                 pass
 
     return render(request, 'wagalytics/dashboard.html', {
-        'pageview': dict_view,
+        'pageview': page_view,
         'visitor': visitor_view,
         'count_pageview': Pageview.objects.count(),
         'count_visitor': Visitor.objects.count(),
