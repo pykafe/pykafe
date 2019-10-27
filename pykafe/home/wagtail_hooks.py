24ba1436869d890c601ba4db866d6b26ab6e331a
Onorio de Jesus Afonso <onorio@catalpa.io>
2019-10-27 22:55:58 +0900

    include logon in login page and other styling change

--- pykafe/home/wagtail_hooks.py
+++ pykafe/home/wagtail_hooks.py
@@ -14,4 +14,15 @@ def make_h1_default(features):
 
 @hooks.register('insert_global_admin_css', order=100)
 def global_admin_css():
-    return format_html('<link rel="stylesheet" href="{}">', static("css/custom.css"))
+    return format_html(
+      '<link rel="stylesheet" href="{}">',
+      static("css/admincustom.css")
+    )
+
+
+@hooks.register('insert_global_admin_css', order=100)
+def pykafe_css():
+    return format_html(
+      '<link rel="stylesheet" href="{}">',
+      static("css/pykafe.css")
+    )
