3fb2ee96dca67d8099c9670f41648fdd69dede9a
Mariano de Deus <dedeus_mariano@yahoo.com>
2019-11-16 13:07:51 +0900

    Hatama media no static ba iha sw

--- pykafe/pykafe/templates/sw.js
+++ pykafe/pykafe/templates/sw.js
@@ -5,12 +5,23 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.3.1/workbox
 
 if (workbox) {
     workbox.routing.registerRoute(
-        new RegExp('/media/.+'),
+        new RegExp('/media/.*'),
         workbox.strategies.staleWhileRevalidate()
       );
 
     workbox.routing.registerRoute(
-        new RegExp('.*'),
-        workbox.strategies.networkFirst()
+        new RegExp('/static/.*'),
+        workbox.strategies.CacheFirst()
+      );
+
+    workbox.routing.registerRoute(
+        new RegExp('/accounts|admin)/.*'),
+        workbox.strategies.NetworkOnly()
     );
+
+    workbox.routing.registerRoute(
+        new RegExp('.*'),
+        workbox.strategies.NetworkFirst()
+    );
+
 }
