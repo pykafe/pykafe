a924b21110ac507f51863418a6972af655628537
joanico <joanico@catalpa.io>
2023-03-28 17:29:08 +0900

    add ajax a global function to fix ajax request error on django 4

--- pykafe/pykafe/settings/base.py
+++ pykafe/pykafe/settings/base.py
@@ -75,6 +75,7 @@ MIDDLEWARE = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'wagtail.contrib.redirects.middleware.RedirectMiddleware',
+    'search.middlewares.AjaxMiddleware', 
 )
 
 ROOT_URLCONF = 'pykafe.urls'
